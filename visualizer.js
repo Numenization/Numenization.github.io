2c39cb852b1ca8ca16720e552ce8d327633c36f0
Numenization <nicholas.spencer5@gmail.com>
2018-10-15 14:59:41 -0400

    Rewrote visualizer to be better

--- visualizer.js
+++ visualizer.js
@@ -1,206 +1,48 @@
-const width = 1000;  // hardcoded value for canvas width
-const height = 800; // hardcoded value for canvas height
-const array_length = 500; // temporarily hardcoded array length
+const length = 50;
+const fps = 60;
+const delay = 1;
+list = [];
+var animator = null;
 
-var canvas = document.getElementById("main_canvas")
-var ctx = canvas.getContext("2d");
-
-var values = [];
-var lineWidth = 1;
-
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-(function() {
-    var timeouts = [];
-    var messageName = "zero-timeout-message";
-
-    // Like setTimeout, but only takes a function argument.  There's
-    // no time argument (always zero) and no arguments (you have to
-    // use a closure).
-    function setZeroTimeout(fn) {
-        timeouts.push(fn);
-        window.postMessage(messageName, "*");
-    }
- 
-    function handleMessage(event) {
-        if (event.source == window && event.data == messageName) {
-            event.stopPropagation();
-            if (timeouts.length > 0) {
-                var fn = timeouts.shift();
-                fn();
-            }
-        }
-    }
-
-    window.addEventListener("message", handleMessage, true);
- 
-    // Add the one thing we want added to the window object.
-    window.setZeroTimeout = setZeroTimeout;
-})();
-
-function fastSleep(ms) {
-	return new Promise(resolve => setZeroTimeout(resolve, ms));
-}
-
-async function setup() {
-	var ratio = height / array_length;
-
-	// populate array with random numbers
-	for(var i = 0; i < array_length; i++) {
-		values.push((i + 1) * ratio);
+function setup() {
+	for(var i = 0; i < length; i++) {
+		list.push(i);
 	}
-
-	drawArray(values);
-	await sleep(500);
-	await shuffle(values);
-	await sleep(500);
-	await selectionSort(values);
+	createCanvas(1000,800);
+	frameRate(fps);
+	animator = new Animator(list, delay);
 }
 
-function drawArray(a) {
-	// draw lines for the values in the array
-	lineWidth = width / values.length;
-	ctx.lineWidth = lineWidth;
-	ctx.strokeStyle = "#FFFFFF";
-	for (var i = 0; i < a.length; i++) {
-		ctx.moveTo(i * lineWidth + lineWidth / 2, height);
-		ctx.lineTo(i * lineWidth + lineWidth / 2, height - a[i]);
-		ctx.stroke();
-	}
-}
+function draw() {
+	background('#222222');
 
-function clearArray(a) {
-	lineWidth = width / values.length;
-	ctx.lineWidth = lineWidth;
-	ctx.strokeStyle = "#000000";
-	for (var i = 0; i < a.length; i++) {
-		ctx.moveTo(i * lineWidth, height);
-		ctx.lineTo(i * lineWidth, height - a[i]);
-		ctx.stroke();
-		ctx.closePath();
-	}
-}
-
-function eraseLine(a, i) {
-	ctx.clearRect(i * lineWidth, 0, lineWidth, height);
-}
-
-async function swap(a, i, j, delay=0) {
-	// clear the lines already there
-	lineWidth = width / a.length;
-	ctx.lineWidth = lineWidth;
-
-	eraseLine(a, i);
-	eraseLine(a, j);
-
-	// actually swap the values
-	var temp = a[i];
-	a[i] = a[j];
-	a[j] = temp;
-
-	// redraw the new lines
-	ctx.strokeStyle = "#FFFFFF";
-	ctx.beginPath();
-	ctx.moveTo(i * lineWidth + lineWidth / 2, height - a[i]);
-	ctx.lineTo(i * lineWidth + lineWidth / 2, height);
-	ctx.stroke();
-	ctx.closePath();
-
-	ctx.beginPath();
-	ctx.moveTo(j * lineWidth + lineWidth / 2, height - a[j]);
-	ctx.lineTo(j * lineWidth + lineWidth / 2, height);
-	ctx.stroke();
-	ctx.closePath();
-
-	if(delay > 0) {
-		await sleep(delay);
+	var lineWidth = width / length;
+	var heightRatio = height / length;
+	if(length > 500) {
+		strokeWeight(0);
 	}
 	else {
-		await sleep(delay);
+		strokeWeight(1);
 	}
+	var arr = animator.getArray();
+	var colors = animator.getColors();
+	for(var i = 0; i < arr.length; i++) {
+		fill(colors[i]);
+		rect(i * lineWidth, height, lineWidth, (-arr[i] - 1) * heightRatio);
+	}	
 }
 
-async function shuffle(a, delay=0) {
-	for(var i = a.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));	
-		await swap(a, i, j, delay);
+function keyPressed() {
+	if(keyCode === 83) {
+		animator.cancel();
+		animatedShuffle(animator.getArray(), animator);
+	}
+	if(keyCode === 49) {
+		animator.cancel();
+		bubbleSort(animator.getArray(), animator);
+	}
+	if(keyCode == 50) {
+		animator.cancel();
+		selectionSort(animator.getArray(), animator);
 	}
 }
-
-async function bubbleSort(a, delay=0) {
-	var startTime = Date.now();
-	for(var i = 0; i < a.length; i++) {
-		for(var j = 0; j < a.length - i - 1; j++) {
-			if(a[j] > a[j + 1]) {
-				await swap(a, j, j + 1, delay);
-			}
-		}
-		//await fastSleep(delay);
-	}
-	var endTime = Date.now();
-	var elapsedTime = endTime - startTime;
-	console.log(elapsedTime);
-}
-
-async function selectionSort(a, delay=0) {
-	var startTime = Date.now();
-	for(var i = 0; i < a.length - 1; i++) {
-		var min = i;
-		for(var j = i + 1; j < a.length; j++) {
-			if(a[min] > a[j]) {
-				min = j;
-			}
-		}
-		await swap(a, i, min, delay);
-	}
-	var endTime = Date.now();
-	var elapsedTime = endTime - startTime;
-	console.log(elapsedTime);
-}
-
-async function partition(a, low, high, delay=0) {
-	var pivot = a[high];
-	var i = low - 1;
-	for(var j = low; j <= high - 1; j++) {
-		if(a[j] <= pivot) {
-			i++;
-			await swap(a, i, j, delay);
-		}
-	}
-
-	await swap(i + 1, high, delay);
-	console.log(high + " BUP " + low);
-	return i + 1;
-}
-
-async function quickSort(a, l, h, delay=0, stack, top) {
-	h = stack[top--];
-	l = stack[top--];
-
-	var p = await partition(a, l, h, delay);
-
-	if(p - 1 > l) {
-		stack[++top] = l;
-		stack[++top] = p - 1;
-	}
-
-	if(p + 1 < h) {
-		stack[++top] = p + 1;
-		stack[++top] = h;
-	}
-}
-
-async function quickSortStart(a, delay=0) {
-	var stack = a;
-	var top = -1;
-	
-	stack[++top] = 0;
-	stack[++top] = a.length - 1;
-	while(top >= 0) {
-		await quickSort(a, 0, a.length - 1, delay, stack, top);
-	}
-} 
-
-setup();
---
+++ visualizerlib.js
@@ -0,0 +1,131 @@
+const BASE_COLOR = "#FFFFFF"
+const SWAP_COLOR = "#FF0000"
+const COMP_COLOR = "#0000FF"
+
+function Animator(a, delay) {
+	this._array = a;
+	this._animatedArray = [];
+	for(var i = 0; i < this._array.length; i++) {
+		this._animatedArray.push(this._array[i]);
+	}
+	this._colors = [];
+	this._queue = [];
+	var _this = this;
+	this._id = window.setInterval(function() {_this._update();}, delay);
+	this._init();
+}
+
+Animator.prototype._init = function() {
+	for(var i = 0; i < this._array.length; i++) {
+		this._colors.push(BASE_COLOR);
+	}
+}
+
+Animator.prototype.resetColors = function() {
+	for(var i = 0; i < this._array.length; i++) {
+		this._colors[i] = BASE_COLOR;
+	}
+}
+
+Animator.prototype._update = function() {
+	// takes the action in the front of the queue and processes it
+	// actions always take the form of ["ACTION_TYPE", i, j]
+	this.resetColors();
+	if(this._queue.length <= 0)
+		return;
+	var action = this._queue.shift(); // takes first action out of queue and moves array over
+	var i = action[1];
+	var j = action[2];
+	if(action[0] === "compare") {
+		this._colors[i] = COMP_COLOR;
+		this._colors[j] = COMP_COLOR;
+	}
+	else if(action[0] === "swap") {
+		this._colors[i] = SWAP_COLOR;
+		this._colors[j] = SWAP_COLOR;
+		var temp = this._animatedArray[i];
+		this._animatedArray[i] = this._animatedArray[j];
+		this._animatedArray[j] = temp;
+	}
+}
+
+Animator.prototype._syncArrays = function() {
+	var length = this._animatedArray.length;
+	for(var i = 0; i < length; i++) {
+		this._array[i] = this._animatedArray[i];
+	}
+}
+
+Animator.prototype.cancel = function() {
+	var length = this._queue.length;
+	if(length === 0)
+		return;
+	for(var i = 0; i < length; i++) {
+		this._queue.pop();
+	}
+	this.resetColors();
+	this._syncArrays();
+}
+
+Animator.prototype.compare = function(i, j) {
+	// returns true if a[i] is greater than a[j] and enqueues a compare action
+	this._queue.push(["compare", i, j]);
+	return (this._array[i] > this._array[j]);
+}
+
+Animator.prototype.swap = function(i, j) {
+	// swaps two numbers in the array and enqueues a swap action
+	this._queue.push(["swap", i, j]);
+	var temp = this._array[i];
+	this._array[i] = this._array[j];
+	this._array[j] = temp;
+}
+
+Animator.prototype.getArray = function() {
+	return this._animatedArray;
+}
+
+Animator.prototype.getColors = function() {
+	return this._colors;
+}
+
+function shuffleList(a, delay=0) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+function animatedShuffle(arr, animator) {
+	var j, i;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        animator.swap(i, j)
+    }
+}
+
+function bubbleSort(arr, animator) {
+	for(var i = 0; i < arr.length; i++) {
+		for(var j = 0; j < arr.length - i - 1; j++) {
+			if(animator.compare(j, j + 1)) {
+				animator.swap(j, j + 1);
+			}
+		}
+	}
+}
+
+function selectionSort(arr, animator) {
+	for(var i = 0; i < arr.length - 1; i++) {
+		var min = i;
+		for(var j = i; j < arr.length; j++) {
+			if(animator.compare(min, j)) {
+				min = j;
+			}
+		}
+		animator.swap(i, min);
+	}
+}
